9d916c99d9c9c0a274c03d2c55546bac7930faad
Dilshan <dopatha@nc.rr.com>
2020-07-16 17:16:09 -0400

    final pushes

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +19,8 @@ mongoose.connect("mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
