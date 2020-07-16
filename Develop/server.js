9a4debe8d8a34e6130437cbe6328c09d55a2feca
Dilshan <dopatha@nc.rr.com>
2020-07-16 17:27:19 -0400

    final push

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
